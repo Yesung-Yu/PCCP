a560c54ada56f4ea991100c0784d58608120c424
nick0 <nick0@DESKTOP-3URB6T1>
2024-02-18 18:10:13 +0900

    "짝수와 홀수"

--- src/Level_1/Even_OddNumbers.java
+++ src/Level_1/Even_OddNumbers.java
@@ -1,10 +1,20 @@
-package Level_1;
+import java.util.Scanner;
 
 public class Even_OddNumbers {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public String solution(int num) {
+        String answer = num % 2 == 0 ? "Even" : "Odd";
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("정수를 입력하세요: ");
+        int num = sc.nextInt();
+        
+        Even_OddNumbers s = new Even_OddNumbers();
+        System.out.println(num + "\t" + s.solution(num));
+        
+        sc.close();
+    }
 }
